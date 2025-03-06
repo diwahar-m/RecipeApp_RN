591c2994287691575cccca4cd17aee7b6fad9597
diwahar-m <msdiwahar5@gmail.com>
2025-03-06 15:09:42 +0530

    root navigation added

--- Server/db/db.ts
+++ Server/db/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectToDB = async (): Promise<void>=> {
     try{
         const connection =await mongoose.connect(process.env.MONGODB_URI as string)
-        console.log(`MongoDb conncted, ${connection.connection.host} `)
+        console.log(`MongoDb connected, ${connection.connection.host} `)
     } catch(err) {
         console.log(err); 
         process.exit(1)
